1f535fe83e6b5d590f3ffaea2febd7259d8acad2
Serena Chen <serenazychen@gmail.com>
2018-08-17 14:59:52 -0400

    verify new homs for triangle

--- varex-hom-finder/src/verify/VerifyTriangle.java
+++ varex-hom-finder/src/verify/VerifyTriangle.java
@@ -7,7 +7,7 @@ import mutated.triangleAll.Triangle_ESTest_improved;
 public class VerifyTriangle {
 
   public static void main(String[] args) {
-    VerifySSHOM sshom = new VerifySSHOM(Triangle.class, Triangle_ESTest_improved.class, "data/found-mutations/alltriangle.txt");
+    VerifySSHOM sshom = new VerifySSHOM(Triangle.class, Triangle_ESTest_improved.class, "data/found-mutations/alltriangle_test.txt");
     sshom.printVerify();
   }
 
