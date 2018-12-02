e211d026a74b40182475e45c20628c7d07746ba5
Jens Meinicke <meinicke@ovgu.de>
2018-12-02 15:16:50 -0500

    add BoundedModelIterator

---
+++ varex-hom-finder/src/solver/sat/BoundedModelIterator.java
@@ -0,0 +1,61 @@
+package solver.sat;
+
+import org.sat4j.core.VecInt;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVecInt;
+import org.sat4j.specs.TimeoutException;
+import org.sat4j.tools.SolverDecorator;
+
+public class BoundedModelIterator extends SolverDecorator<ISolver> {
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean trivialfalsity = false;
+
+	private final int numVariables;
+
+    public BoundedModelIterator(ISolver solver, int numVariables) {
+        super(solver);
+        this.numVariables = numVariables;
+    }
+
+    @Override
+    public int[] model() {
+        int[] last = super.model();
+        IVecInt clause = new VecInt(last.length);
+        for (int i = 0; i < numVariables; i++) {
+        	clause.push(-last[i]);
+        }
+        try {
+            addClause(clause);
+        } catch (ContradictionException e) {
+            trivialfalsity = true;
+        }
+        return last;
+    }
+
+    @Override
+    public boolean isSatisfiable() throws TimeoutException {
+        if (trivialfalsity) {
+            return false;
+        }
+        trivialfalsity = false;
+        return super.isSatisfiable(true);
+    }
+
+    @Override
+    public boolean isSatisfiable(IVecInt assumps) throws TimeoutException {
+        if (trivialfalsity) {
+            return false;
+        }
+        trivialfalsity = false;
+        return super.isSatisfiable(assumps,true);
+    }
+
+    @Override
+    public void reset() {
+        trivialfalsity = false;
+        super.reset();
+    }
+}
